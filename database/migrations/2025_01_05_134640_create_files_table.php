fb7cbc7470808eb4751c7436c2c16165c5e3036f
mezentsevs <mezentsevs@rambler.ru>
2025-01-05 19:09:34 +0400

    Add create folders and files tables migrations

---
+++ database/migrations/2025_01_05_134640_create_files_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('folder_id')->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->text('content')->nullable();
+            $table->integer('size')->nullable()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
