b89e00711234e19b9cfed01d26000a9e07f964d1
mezentsevs <mezentsevs@rambler.ru>
2025-01-31 16:53:22 +0400

    Add transition fill to base image button

--- resources/js/Components/BaseImageButton.vue
+++ resources/js/Components/BaseImageButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fill-indigo-300 dark:fill-indigo-700 hover:fill-indigo-500 active:fill-indigo-700 dark:active:fill-indigo-300 cursor-pointer">
+    <div class="fill-indigo-300 dark:fill-indigo-700 hover:fill-indigo-500 active:fill-indigo-700 dark:active:fill-indigo-300 cursor-pointer transition-[fill]">
         <slot />
     </div>
 </template>
