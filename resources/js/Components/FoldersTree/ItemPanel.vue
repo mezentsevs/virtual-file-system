8983702eaa17ef86c42569f5a238ab8499c04340
mezentsevs <mezentsevs@rambler.ru>
2025-02-05 17:35:12 +0400

    Refactoring

--- resources/js/Components/FoldersTree/ItemPanel.vue
+++ resources/js/Components/FoldersTree/ItemPanel.vue
@@ -3,7 +3,7 @@ import { inject } from 'vue';
 import FolderPanel from '@/Components/FoldersTree/FolderPanel.vue';
 import FilePanel from '@/Components/FoldersTree/FilePanel.vue';
 
-const emit = defineEmits([
+defineEmits([
     'folderDeleted',
     'fileDeleted',
 ]);
@@ -13,7 +13,7 @@ const item = inject('currentItem');
 
 <template>
     <div class="item-panel m-4 ml-0 p-4 w-2/3 min-w-[400px] min-h-[600px] text-gray-500 dark:text-gray-400 border-solid border-[1px] border-gray-500 dark:border-gray-400 rounded-lg">
-        <FolderPanel v-if="item.type === 'folder'" @folder-deleted="emit('folderDeleted')" />
-        <FilePanel v-else-if="item.type === 'file'" @file-deleted="emit('fileDeleted')" />
+        <FolderPanel v-if="item.type === 'folder'" @folder-deleted="$emit('folderDeleted')" />
+        <FilePanel v-else-if="item.type === 'file'" @file-deleted="$emit('fileDeleted')" />
     </div>
 </template>
