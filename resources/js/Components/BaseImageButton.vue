1227e6cd13750178a3a25aca71b259df15ec07dd
mezentsevs <mezentsevs@rambler.ru>
2025-02-10 21:36:51 +0400

    Add bounce animation on base image button active

--- resources/js/Components/BaseImageButton.vue
+++ resources/js/Components/BaseImageButton.vue
@@ -1,5 +1,29 @@
 <template>
-    <div class="fill-indigo-300 dark:fill-indigo-700 hover:fill-indigo-500 active:fill-indigo-700 dark:active:fill-indigo-300 cursor-pointer transition-[fill]">
+    <div class="image-button fill-indigo-300 dark:fill-indigo-700 hover:fill-indigo-500 active:fill-indigo-700 dark:active:fill-indigo-300 cursor-pointer transition-[fill]">
         <slot />
     </div>
 </template>
+
+<style scoped>
+.image-button:active {
+    animation: bounce 0.5s;
+}
+
+@keyframes bounce {
+    0% {
+        transform: scale(0);
+    }
+
+    25% {
+        transform: scale(1.1);
+    }
+
+    50% {
+        transform: scale(1.25);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+}
+</style>
