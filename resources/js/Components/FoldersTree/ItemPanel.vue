3276c6b48f52d359a444216608435cd8fd329a76
mezentsevs <mezentsevs@rambler.ru>
2025-02-20 19:11:05 +0400

    Update item panel div to section

--- resources/js/Components/FoldersTree/ItemPanel.vue
+++ resources/js/Components/FoldersTree/ItemPanel.vue
@@ -12,8 +12,8 @@ const item = inject('currentItem');
 </script>
 
 <template>
-    <div class="item-panel m-4 ml-0 p-4 w-2/3 min-w-[400px] min-h-[600px] text-gray-500 dark:text-gray-400 border-solid border-[1px] border-gray-500 dark:border-gray-400 rounded-lg">
+    <section class="item-panel m-4 ml-0 p-4 w-2/3 min-w-[400px] min-h-[600px] text-gray-500 dark:text-gray-400 border-solid border-[1px] border-gray-500 dark:border-gray-400 rounded-lg">
         <FolderPanel v-if="item.type === 'folder'" @folder-deleted="$emit('folderDeleted')" />
         <FilePanel v-else-if="item.type === 'file'" @file-deleted="$emit('fileDeleted')" />
-    </div>
+    </section>
 </template>
