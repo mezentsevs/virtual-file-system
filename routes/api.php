2eff1ff48c61dca98b94bcc6b3b5bdaa18a89878
mezentsevs <mezentsevs@rambler.ru>
2025-03-16 10:52:42 +0400

    Refactoring

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/folders', [FolderController::class, 'index'])->name('folders.index');
-    Route::post('/folders', [FolderController::class, 'store'])->name('folders.create');
-    Route::patch('/folders/{folder}', [FolderController::class, 'update'])->name('folders.update');
-    Route::delete('/folders/{folder}', [FolderController::class, 'destroy'])->name('folders.delete');
+    Route::controller(FolderController::class)->group(function () {
+        Route::get('/folders', 'index')->name('folders.index');
+        Route::post('/folders', 'store')->name('folders.create');
+        Route::patch('/folders/{folder}', 'update')->name('folders.update');
+        Route::delete('/folders/{folder}', 'destroy')->name('folders.delete');
+    });
 
-    Route::post('/files', [FileController::class, 'store'])->name('files.create');
-    Route::patch('/files/{file}', [FileController::class, 'update'])->name('files.update');
-    Route::delete('/files/{file}', [FileController::class, 'destroy'])->name('files.delete');
+    Route::controller(FileController::class)->group(function () {
+        Route::post('/files', 'store')->name('files.create');
+        Route::patch('/files/{file}', 'update')->name('files.update');
+        Route::delete('/files/{file}', 'destroy')->name('files.delete');
+    });
 
     Route::get('/user', function (Request $request) {
         return $request->user();
