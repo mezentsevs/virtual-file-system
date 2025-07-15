c75e9e437f3944b6cb5ec64584f34b2f6c036efb
mezentsevs <mezentsevs@rambler.ru>
2025-07-15 14:31:23 +0400

    Refactor item panel component

--- resources/js/Components/FoldersTree/ItemPanel.vue
+++ resources/js/Components/FoldersTree/ItemPanel.vue
@@ -1,7 +1,13 @@
 <template>
-    <section class="item-panel m-4 ml-0 p-4 w-2/3 min-w-[400px] min-h-[600px] text-gray-500 dark:text-gray-400 border-solid border-[1px] border-gray-500 dark:border-gray-400 rounded-lg">
-        <FolderPanel v-if="item.type === 'folder'" @folder-deleted="$emit('folderDeleted')" />
-        <FilePanel v-else-if="item.type === 'file'" @file-deleted="$emit('fileDeleted')" />
+    <section class="item-panel w-2/3 min-w-[400px] min-h-[600px] m-4 ml-0 p-4 border border-solid border-gray-500 dark:border-gray-400 text-gray-500 dark:text-gray-400 rounded-lg">
+        <FolderPanel
+            v-if="item.type === 'folder'"
+            @folder-deleted="$emit('folderDeleted')"
+        />
+        <FilePanel
+            v-else-if="item.type === 'file'"
+            @file-deleted="$emit('fileDeleted')"
+        />
     </section>
 </template>
 
