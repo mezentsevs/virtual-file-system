eead29068d83a74479025feb01c1638d5a0d45ac
mezentsevs <mezentsevs@rambler.ru>
2025-07-17 09:29:03 +0400

    Refactor dashboard component

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,14 +1,14 @@
 <template>
     <AppLayout title="Dashboard">
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            <h2 class="text-gray-800 dark:text-gray-200 text-xl font-semibold leading-tight">
                 {{ appName }}
             </h2>
         </template>
 
         <div class="py-12" v-if="isLoaded">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden sm:rounded-lg shadow-xl">
                     <FoldersTree :folders="foldersStore.folders" />
                 </div>
             </div>
