7ba2eedfdb7044e46bb703b6973d46746d486f81
mezentsevs <mezentsevs@rambler.ru>
2025-07-17 10:02:13 +0400

    Refactor welcome component

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -5,7 +5,7 @@
             <small>v{{ appVersion }}</small>
         </div>
 
-        <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#818DF8] selection:text-white">
+        <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-indigo-400 selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                 <header class="min-w-[460px] grid grid-cols-2 items-center gap-2 py-2 lg:grid-cols-3 bg-white dark:bg-gray-800 rounded-lg shadow">
                     <div class="flex lg:justify-center lg:col-start-2">
@@ -21,7 +21,7 @@
                         <Link
                             v-if="$page.props.auth.user"
                             :href="route('dashboard')"
-                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#818DF8] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-indigo-400 dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                         >
                             Dashboard
                         </Link>
@@ -29,7 +29,7 @@
                         <template v-else>
                             <Link
                                 :href="route('login')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#818DF8] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-indigo-400 dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                             >
                                 Log in
                             </Link>
@@ -37,7 +37,7 @@
                             <Link
                                 v-if="canRegister"
                                 :href="route('register')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#818DF8] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-indigo-400 dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                             >
                                 Register
                             </Link>
@@ -48,7 +48,7 @@
                 <main class="min-h-96 mt-6 text-center text-black dark:text-white/70 flex flex-col justify-center">
                     <p>
                         This is a
-                        <span class="font-medium text-[#818DF8]">virtual file system</span>,
+                        <span class="font-medium text-indigo-400">virtual file system</span>,
                         written in and for educational and demonstrational purposes.
                     </p>
                 </main>
