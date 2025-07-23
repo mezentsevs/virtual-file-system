e3e48c12bbdde71bf3037102f21de20406cdd6a9
mezentsevs <mezentsevs@rambler.ru>
2025-07-23 20:50:04 +0400

    Remove unused handle image error function

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -87,10 +87,4 @@ defineProps({
         required: true,
     },
 });
-
-const handleImageError = () => {
-    document.getElementById('screenshot-container')?.classList.add('!hidden');
-    document.getElementById('docs-card')?.classList.add('!row-span-1');
-    document.getElementById('docs-card-content')?.classList.add('!flex-row');
-};
 </script>
