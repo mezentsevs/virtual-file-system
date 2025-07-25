decdda91f99ad6db21f238701428109c90770e0c
mezentsevs <mezentsevs@rambler.ru>
2025-07-25 21:14:27 +0400

    Add eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,137 @@
+import vueParser from 'vue-eslint-parser';
+import eslintPluginVue from 'eslint-plugin-vue';
+import eslintPluginImport from 'eslint-plugin-import';
+import eslintPluginPromise from 'eslint-plugin-promise';
+import globals from 'globals';
+
+export default [
+    {
+        ignores: [
+            'dist/**',
+            'node_modules/**',
+            'public/**'
+        ]
+    },
+
+    {
+        files: ['**/*.vue'],
+        languageOptions: {
+            ecmaVersion: 2025,
+            sourceType: 'module',
+            globals: globals.browser,
+            parser: vueParser,
+            parserOptions: {
+                parser: {
+                    js: '@babel/eslint-parser',
+                    ts: '@typescript-eslint/parser',
+                },
+                requireConfigFile: false,
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        },
+        plugins: {
+            vue: eslintPluginVue
+        },
+        rules: {
+            'vue/html-self-closing': ['error', {
+                html: {
+                    void: 'never',
+                    normal: 'always',
+                    component: 'always'
+                }
+            }],
+            'vue/attributes-order': ['error', {
+                order: [
+                    'DEFINITION',
+                    'LIST_RENDERING',
+                    'CONDITIONALS',
+                    'RENDER_MODIFIERS',
+                    'GLOBAL',
+                    'UNIQUE',
+                    'TWO_WAY_BINDING',
+                    'OTHER_DIRECTIVES',
+                    'OTHER_ATTR',
+                    'EVENTS',
+                    'CONTENT'
+                ],
+                alphabetical: false
+            }],
+            'vue/order-in-components': ['error', {
+                order: [
+                    'el',
+                    'name',
+                    'key',
+                    'parent',
+                    'functional',
+                    ['delimiters', 'comments'],
+                    ['components', 'directives', 'filters'],
+                    'extends',
+                    'mixins',
+                    ['provide', 'inject'],
+                    'ROUTER_GUARDS',
+                    'layout',
+                    'middleware',
+                    'validate',
+                    'scrollToTop',
+                    'transition',
+                    'loading',
+                    'inheritAttrs',
+                    'model',
+                    ['props', 'propsData'],
+                    'emits',
+                    'setup',
+                    'asyncData',
+                    'data',
+                    'fetch',
+                    'head',
+                    'computed',
+                    'watch',
+                    'watchQuery',
+                    'methods',
+                    ['template', 'render'],
+                    'renderError'
+                ]
+            }]
+        }
+    },
+
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2025,
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            }
+        },
+        plugins: {
+            import: eslintPluginImport,
+            promise: eslintPluginPromise
+        },
+        rules: {
+            'no-console': 'warn',
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            'import/order': [
+                'error',
+                {
+                    groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                    'newlines-between': 'always'
+                }
+            ]
+        },
+        settings: {
+            'import/resolver': {
+                alias: {
+                    map: [
+                        ['@', './resources/js'],
+                        ['~', './resources']
+                    ],
+                    extensions: ['.js', '.vue', '.json']
+                }
+            }
+        }
+    }
+];
